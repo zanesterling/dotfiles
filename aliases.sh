d13349b3987100549f544618e913f23da87d0d5e
Zane Sterling <sterling.zane@gmail.com>
2013-12-31 22:13:11 -0500

    Add some of the stuff

---
+++ aliases.sh
@@ -0,0 +1,28 @@
+alias ls='ls --color'
+alias ll='ls -l'
+alias la='ls -la'
+
+alias rld='source ~/config/aliases.sh'
+alias fix='vim ~/config/aliases.sh'
+
+alias cc='source ~/.tmux/start-cc'
+alias cw='source ~/.tmux/start-wc'
+alias e='source ~/.tmux/end'
+alias tit='source ~/.tmux/std-setup'
+
+alias sys='cd ~/hacking/systems'
+alias sd='cd ~/hacking/softdev'
+
+alias gp='git push'
+alias gl='git pull'
+alias ga='git add'
+alias gc='git commit'
+alias gs='git status -s'
+alias gd='git diff'
+alias gr='git rm'
+alias gcl='git clone'
+
+alias spfy='spotify 2> /dev/null > /dev/null &'
+alias groove='vlc ~/music/groovesalad.pls 2> /dev/null > /dev/null &'
+alias flux='~/tools/xflux'
+alias stm='steam 2> /dev/null > /dev/null &'
