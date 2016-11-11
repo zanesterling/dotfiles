823ae114ab5dd96e51192575b57719c006ab8336
Zane Sterling <sterling.zane@gmail.com>
2016-11-10 20:17:43 -0500

    Get rid of "Hit ENTER" prompt on journal open

--- open-journal.sh
+++ open-journal.sh
@@ -2,5 +2,6 @@
 # Cron script for 30-min activity journal
 
 if [ -f /Users/zane/config/checked-in ]; then
-	mvim -c "call vimwiki#diary#make_note(v:count1)" + -c 'r !date "+\%n= \%H:\%M =\%n"'
+	cd ~/vimwiki
+	mvim -c "silent call vimwiki#diary#make_note(v:count1)" + -c 'r !date "+\%n= \%H:\%M =\%n"'
 fi
