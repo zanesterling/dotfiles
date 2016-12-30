52166a1a2bea58799b8b9c691e218e0833946e04
Zane Sterling <sterling.zane@gmail.com>
2016-12-30 14:54:03 -0500

    fix path issues

--- open-journal.sh
+++ open-journal.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Cron script for 30-min activity journal
 
+export PATH=$PATH:/usr/local/bin
 if [ -f /Users/zane/config/checked-in ]; then
 	cd ~/vimwiki
 	mvim -c "silent call vimwiki#diary#make_note(v:count1)" + -c 'r !date "+\%n= \%H:\%M =\%n"'
