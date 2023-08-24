164e6909c5dcd3af84af2851150a2458ae51f2c8
Zane Sterling <sterling.zane@gmail.com>
2023-08-24 17:56:45 +0200

    Document what's already in setup.sh.

--- setup.sh
+++ setup.sh
@@ -4,9 +4,11 @@ read -a DOTFILES <<< $(cat homedir-files)
 
 dryrun=true
 
+# Move files indicated in homedir-files into the home directory.
 for path in ${DOTFILES[@]}
 do
 	cp -R $path $HOME/$path
 done
 
+# If it doesn't already exist, make a file for local ZSH config.
 touch $HOME/.zsh_local
