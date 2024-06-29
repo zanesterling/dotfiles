e8b4af12d94330e00dc08ba78c427cd23e460f42
Zane Sterling <sterling.zane@gmail.com>
2024-06-29 08:42:49 +0000

    Make echo commands bolded

--- setup.sh
+++ setup.sh
@@ -4,61 +4,64 @@ read -a DOTFILES <<< $(cat homedir-files)
 
 dryrun=true
 
+bold=$(tput bold)
+normal=$(tput sgr0)
 
-echo 1.0 Basic setup
-echo 1.1 Move files indicated in homedir-files into the home directory.
+
+echo "${bold}1.0 Basic setup${normal}"
+echo "${bold}1.1 Move files indicated in homedir-files into the home directory.${normal}"
 for path in ${DOTFILES[@]}
 do
 	cp -R $path $HOME/$path
 done
 mkdir $HOME/tmp
 
-echo "1.2 If it doesn't already exist, make a file for local ZSH config."
+echo "${bold}1.2 If it doesn't already exist, make a file for local ZSH config.${normal}"
 touch $HOME/.zsh_local
 
 
-echo 2.0 Vim setup
-echo 2.1 Install Vundle.
+echo "${bold}2.0 Vim setup${normal}"
+echo "${bold}2.1 Install Vundle.${normal}"
 if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]
 then
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
-echo 2.2 Install packages using Vundle.
+echo "${bold}2.2 Install packages using Vundle.${normal}"
 vim +PluginInstall +qall
 
 
-echo 3.0 ZSH setup
-echo 3.1 Create directory for logs.
+echo "${bold}3.0 ZSH setup${normal}"
+echo "${bold}3.1 Create directory for logs.${normal}"
 mkdir -p ~/.logs
 
-echo 3.2 Install needed packages.
+echo "${bold}3.2 Install needed packages.${normal}"
 sudo apt install python tmux
 
-echo 3.3 Set up zsh-git-prompt.
+echo "${bold}3.3 Set up zsh-git-prompt.${normal}"
 mkdir -p ~/.zsh/
 if [ ! -d ~/.zsh/zsh-git-prompt ]
 then
 	git clone http://github.com/zsh-git-prompt/zsh-git-prompt ~/.zsh/zsh-git-prompt
 fi
 
-echo "4.0 Git stuff -- make sure we're on the newest version!"
+echo "${bold}4.0 Git stuff -- make sure we're on the newest version!${normal}"
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt update
 sudo apt install git
 
-echo 5.0 Neovim setup
-echo Install Neovim.
+echo "${bold}5.0 Neovim setup${normal}"
+echo "${bold}Install Neovim.${normal}"
 wget -P $HOME/tmp https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
 tar -C $HOME/tmp -zxvf $HOME/tmp/nvim-linux64.tar.gz
 mkdir -p $HOME/.local/bin
 mv $HOME/tmp/nvim-linux64 $HOME/.local/bin/
 rm $HOME/tmp/nvim-linux64.tar.gz
 
-echo Set up nvim config.
+echo "${bold}Set up nvim config.${normal}"
 mkdir -p $HOME/.config
 cp -R .config/nvim $HOME/.config
 
-echo Install nvim plugins.
+echo "${bold}Install nvim plugins.${normal}"
 nvim +PluginInstall +qall
 
