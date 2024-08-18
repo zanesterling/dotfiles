06967e980d100890490225724f27d585888e8ec3
Zane Sterling <sterling.zane@gmail.com>
2024-08-18 15:59:15 +0200

    Replaced long ago with setup.sh

--- runme.sh
+++
@@ -1,8 +0,0 @@
-mkdir ~/dev
-
-sudo apt install tmux
-brew install tmux
-
-# Set up Vundle et al for Vim.
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
