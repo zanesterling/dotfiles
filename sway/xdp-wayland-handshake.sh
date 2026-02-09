d884c9dc065125572f43f1723e88feeb4444266a
Zane Sterling <sterling.zane@gmail.com>
2026-02-09 15:25:07 +0100

    Fix up the xdp-wayland-handshake script

--- sway/xdp-wayland-handshake.sh
+++ sway/xdp-wayland-handshake.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+set -x
+
 # Source: https://github.com/emersion/xdg-desktop-portal-wlr/wiki/%22It-doesn't-work%22-Troubleshooting-Checklist
 #
 # Makes sure that xdg-desktop-portal knows about the sway Wayland socket.
 # This enables the former to ask the latter for screensharing.
 
 # Import the WAYLAND_DISPLAY env var from sway into the systemd user session.
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=$compositor_name
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
 
 # Stop any services that are running, so that they receive the new env var when they restart.
-systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-systemctl --user start pipewire-media-session
+systemctl --user stop pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-wlr
+systemctl --user start wireplumber xdg-desktop-portal
